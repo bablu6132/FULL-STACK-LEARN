e3d518defc552df3427df9059a1192a68d8522f3
Yalam Panindhar Reddy <ypanindher@gmail.com>
2026-08-05 15:25:32 +0530

    0508

--- WEEL-3/listen.js
+++ WEEL-3/listen.js
@@ -1,2 +1,19 @@
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
 
+console.log("==============================");
+console.log("NODE.JS CUSTOM EVENTS DEMONSTRATION");
+console.log("==============================\n");
+console.log("Welcome to Full Stack Development Laboratory\n");
 
+eventEmitter.on("studentLogin", (name) => {
+    console.log("Student Login Event Triggered");
+    console.log("Student Name:", name);
+});
+
+eventEmitter.on("studentLogin", () => {
+    console.log("Attendance Marked Successfully.");
+    console.log("Welcome to Full Stack Development Laboratory.");
+});
+
+eventEmitter.emit("studentLogin", "pani pixel");
